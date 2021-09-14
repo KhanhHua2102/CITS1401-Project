5197ac508f67dd8da88678f3a8f9a1de40a7e54f
Quang Khanh Hua <Quang.khanh2102@gmail.com>
2021-09-14 12:23:35 +0700

    update

--- 22928469.py
+++ 22928469.py
@@ -35,8 +35,8 @@ def main(inputFile,queryLocId,d1,d2):
     yPos = headerPos[2]
     categoryPos = headerPos[3]
 
-    # compare the locId input with locId in locationList to add x, y, category of that locId to a list
-    def compareLoc(locId):
+    # compare the locId input with locId in locationList to add x, y, category element of that locId to a list
+    def element(locId):
         outputList = []
         for location in locationList:
             if locId.lower() == location[locIdPos].lower():
@@ -46,9 +46,9 @@ def main(inputFile,queryLocId,d1,d2):
         return outputList
 
     # process raw parameters to handle exceptions
-    x1 = compareLoc(queryLocId)[0]
-    y1 = compareLoc(queryLocId)[1]
-    mainCategory = compareLoc(queryLocId)[2]
+    x1 = element(queryLocId)[0]
+    y1 = element(queryLocId)[1]
+    mainCategory = element(queryLocId)[2]
     d1 = float(d1)
     d2 = float(d2)
 
@@ -57,7 +57,7 @@ def main(inputFile,queryLocId,d1,d2):
         return x1 - d1 < x2 < x1 + d1 and y1 - d2 < y2 < y1 + d2
 
     # handle duplicated queryLocId and missing data
-    def inCorretedData(location):
+    def isCorretedData(location):
         return location[locIdPos].lower() == queryLocId.lower() or location[locIdPos][1:] == '' or location[locIdPos] == '' or location[xPos] == '' or location[yPos] == '' or location[categoryPos] == '' or location[locIdPos].lower() == "n/a"  or location[xPos].lower() == "n/a" or location[yPos].lower() == "n/a" or location[categoryPos].lower() == "n/a"
 
     # return locList list
@@ -65,7 +65,7 @@ def main(inputFile,queryLocId,d1,d2):
     def locListFunc():
         locListStrip = []
         for location in locationList:
-            if inCorretedData(location):
+            if isCorretedData(location):
                 continue
             x2 = float(location[xPos])
             y2 = float(location[yPos])
@@ -81,7 +81,7 @@ def main(inputFile,queryLocId,d1,d2):
     def simLocListFunc():
         simLocListStrip = []
         for locId in locList:
-            if compareLoc(locId)[2].lower().strip() == mainCategory:
+            if element(locId)[2].lower().strip() == mainCategory:
                 simLocList.append(locId)
         # output strip list
         for locId in simLocList:
@@ -96,8 +96,8 @@ def main(inputFile,queryLocId,d1,d2):
     distSorted = []
     def distSortedFunc():
         for locId in simLocList:
-            x2  = compareLoc(locId)[0]
-            y2  = compareLoc(locId)[1]
+            x2  = element(locId)[0]
+            y2  = element(locId)[1]
             distance = distanceFunc(x1, x2, y1, y2)
             distSorted.append(distance)
         return sorted(distSorted)
